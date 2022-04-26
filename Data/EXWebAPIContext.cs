e11186465077edfbaf0336dc0556c0658b90d918
Musik88 <piotrmusinski@gmail.com>
2022-04-26 21:39:49 +0200

    Add project files.

---
+++ Data/EXWebAPIContext.cs
@@ -0,0 +1,20 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EXWebAPI.Model;
+
+namespace EXWebAPI.Data
+{
+    public class EXWebAPIContext : DbContext
+    {
+        public EXWebAPIContext (DbContextOptions<EXWebAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EXWebAPI.Model.Product> Product { get; set; }
+    }
+}
---
+++ Model/Product.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EXWebAPI.Model
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required]
+        public DateTime ProductCreated { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The product name cannot exceed {1} characters. ")]
+        public string? ProductName { get; set; }
+
+        [StringLength(200, ErrorMessage = "The product description cannot exceed {1} characters. ")]
+        public string? ProductDescription { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal ProductPrice { get; set; }
+    }
+}
+
